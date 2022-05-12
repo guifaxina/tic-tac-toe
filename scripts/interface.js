9ff35c1f1c4d981f66f9bcc9f52799e2715bdc3e
guifaxina <99680960+guifaxina@users.noreply.github.com>
2022-05-12 08:06:10 -0300

    Add Feature: GameOver
    The game now ends when someone wins.

--- scripts/interface.js
+++ scripts/interface.js
@@ -1,27 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let squares = document.querySelectorAll(".sqr");
-
+  const squares = document.querySelectorAll(".sqr");
   squares.forEach((e) => {
     e.addEventListener("click", handleClick);
   });
 });
 
 function handleClick(event) {
-  let square = event.target;
-  let position = square.id;
-  handleMove(position);
+  handleMove(event.target.id);
   render();
 }
 
 function render() {
-  let squares = document.querySelectorAll(".sqr");
-
-  squares.forEach((square) => {
-    let position = square.id;
+  const squares = document.querySelectorAll(".sqr");
+  squares.forEach((e) => {
+    let position = e.id;
     let symbol = board[position];
 
-    if (symbol != "") {
-      square.innerHTML = `<div class='${symbol}'></div>`;
+    if (e != "") {
+      e.innerHTML = `<div class="${symbol}"></div>`;
     }
   });
 }
